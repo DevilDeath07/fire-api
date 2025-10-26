2f82e680afa01e4b980c5a0dc08d52736e2b13fb
DevilDeath07 <balamurugankumar880@gmail.com>
2025-10-26 17:18:18 +0530

    connect to mongodb with dotenv

--- README.md
+++ README.md
@@ -12,3 +12,8 @@ react app
 4. `git add .`
 5. `git commit -m "you message"`
 6. `git push -u origin main` or  `git push`
+
+### Database options
+
+1. SQL - (MySQL, MariaDB, postgreSQL)
+2. NoSQL - (MongoDB)
--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import router from './routes/crackers.route.js';
+import connectDB from './lib/db.js';
 const app = express();
 const port = 3000;
+//connect to mongodb
+connectDB();
 app.get('/',(req,res) =>{
     res.json({mes : "HelloW Bala!"});
 });
